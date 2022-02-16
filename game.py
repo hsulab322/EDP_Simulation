225e11c6c07270b33f3b298e557715fb39857dbd
Wei-Lun Lin <weilun9539ny@gmail.com>
2022-02-16 18:13:09 +0800

    Change the result output format (keeping the previous one)

--- game.py
+++ game.py
@@ -4,33 +4,39 @@ import random
  
 class Game:
     # Game's attributes
-    def __init__(self, base_value = 200, single_run = None, initial_chip_ratio = 1):
+    def __init__(self, base_value = 200, initial_chip_ratio = 2, exp_type = 0, single_run = 0):
         self.__base_value = base_value
         self.__initial_value_list = [self.__base_value + 100*i for i in range(0, 6)]*2
         self.__ratio_list = (0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 1.1, 1.3, 1.5)
         self.__n_run = len(self.__initial_value_list)
         self.__n_trial = len(self.__ratio_list)
+        self.__exp_type = exp_type
         self.__single_run = single_run
         self.__run_list = list()
         self.__conflict = bool()
         self.__current_run = int()
         self.__current_trial = int()
-        self.__current_gain = float()
-        self.__current_loss = float()
+        self.__current_gain_utility = float()
+        self.__current_loss_utility = float()
+        self.__current_gain_outcome = float()
+        self.__current_loss_outcome = float()
         self.__initial_chip_ratio = initial_chip_ratio
         self.__current_win_or_lose = str()
         self.__x = pd.DataFrame({  # Store trial data
             'Run':[],
             'Trial':[],
-            'Gain':[],
-            'Loss':[],
+            'Gain_outcome':[],
+            'Loss_outcome':[],
+            'Gain_utility':[],
+            'Loss_utility':[],
             'Probability':[],
+            'Conflict_or_not':[],
             'Bet_or_quit':[],
             'Win_or_lose':[],
             'Chip':[],
-            'Incentive':[]
+            'Incentive':[],
         })
-    
+
     # Prepare experiment material:
     # Random runs and initial value (make initial gain present randomly)
     def initialization(self):
@@ -38,8 +44,8 @@ class Game:
         sequence_1 = [i for i in range(0, 6)]
         sequence_2 = [i for i in range (6, 12)]
         
-        # Shuffle runs when they are randomly presented
-        if self.__single_run == None:
+        # When the experiment type is "whole experiment", shuffle runs when they are randomly presented
+        if self.__exp_type == 0:
             random.shuffle(sequence_1)
             random.shuffle(sequence_2)
             while sequence_1[-1] == sequence_2[0]:
@@ -47,8 +53,8 @@ class Game:
                 
             # Combine the two lists
             self.__run_list = sequence_1 + sequence_2
-
-        else:
+        # When the experiment type is "single run", only undergo the specific run
+        elif self.__exp_type == 1:
             self.__run_list = [self.__single_run]
             self.__n_run = 1
 
@@ -56,9 +62,9 @@ class Game:
     def __compute_loss_outcome(self, player):
         __player_attributes_list = player.get_utility_parameters()
         __ratio = self.__ratio_list[self.__current_trial]
-        __gain_utility = (self.__current_gain / __player_attributes_list[2])**__player_attributes_list[0]
-        __loss_utility = __gain_utility * __ratio
-        __loss_outcome = round(-1 * __player_attributes_list[3] * __loss_utility **(1/__player_attributes_list[1]))
+        self.__current_gain_utility = (self.__current_gain_outcome / __player_attributes_list[2])**__player_attributes_list[0]
+        self.__current_loss_utility = self.__current_gain_utility * __ratio
+        __loss_outcome = round(-1 * __player_attributes_list[3] * self.__current_loss_utility **(1/__player_attributes_list[1]))
         return __loss_outcome
 
     # Start the game
@@ -67,40 +73,49 @@ class Game:
         if self.__current_trial == 0:
             __random_run_index = self.__run_list[self.__current_run]
             __initial_value = self.__initial_value_list[__random_run_index]
-            self.__current_gain = __initial_value
-            self.__current_loss = self.__compute_loss_outcome(player)
+            self.__current_gain_outcome = __initial_value
+            self.__current_loss_outcome = self.__compute_loss_outcome(player)
             player.change_current_incentive(None)
-            player.change_current_chip(self.__current_gain*self.__initial_chip_ratio)  # initial chip: equals to initial value
+            player.change_current_chip(self.__current_gain_outcome*self.__initial_chip_ratio)  # initial chip: equals to initial value
         else:
-            self.__current_gain = round(player.get_current_chip()*0.5 + self.__current_gain)
-            self.__current_loss = self.__compute_loss_outcome(player)
+            self.__current_gain_outcome = round(player.get_current_chip()*0.5 + self.__current_gain_outcome)
+            self.__current_loss_outcome = self.__compute_loss_outcome(player)
+
+        # See if the lottery induce conflict
+        self.__check_conflict(player)
         
-        return [self.__current_gain, self.__current_loss]
+        return [self.__current_gain_outcome, self.__current_loss_outcome]
     
     # Check if the player confront with conflict
-    def check_conflict(self, player):
-        gain = self.__current_gain
-        loss = abs(self.__current_loss)
-
-        if player.get_current_chip() <= 0:  # Rule 1: if the player is bankrupt, there is no need to check whether the player is conflict or not
-            self.__conflict = True
-        elif loss == 0:  # Rule 2: loss-outcome equals to 0 means the player may not feel conflict
-            self.__conflict = False
+    def __check_conflict(self, player):
+        gain = self.__current_gain_outcome
+        loss = abs(self.__current_loss_outcome)  # Make the loss outcome > 0, which is easier to compute
+        '''
+        Rule 1: If the player is bankrupt, it is meaningless to know if a player feels conflict.
+        Rule 2: When loss outcome euqals to 0, it means a player can easily choose to bet without conflict.
+        Rule 3: When gain utility and loss utility are the same, and loss outcome is less than 1/3 of the 
+            gain outcome, it means a player cannot feel conflict according to our definition or assumption.
+        '''
+        if player.get_current_chip() <= 0:
+            self.__conflict = 'Meaningless'
+        elif loss == 0:
+            self.__conflict = 'Not_conflict'
         elif self.__ratio_list[self.__current_trial] == 1 and loss/gain < 1/3:
-            # Rule 3: when gain utility and loss utility are the same, the loss-outcome is less than 1/3 of the gain-outcome
-            self.__conflict = False
+            self.__conflict = 'Not-conflict'
         else:
-            self.__conflict = True
-        return self.__conflict
+            self.__conflict = 'Conflict'
     
     # Save the current situation
     def __save_trial(self, player):
         self.__x = self.__x.append({
             'Run':self.__current_run,
             'Trial':self.__current_trial,
-            'Gain':self.__current_gain,
-            'Loss':self.__current_loss,
+            'Gain_outcome':self.__current_gain_outcome,
+            'Loss_outcome':self.__current_loss_outcome,
+            'Gain_utility':self.__current_gain_utility,
+            'Loss_utility':self.__current_loss_utility,
             'Probability':player.get_current_probability(),
+            'Conflict_or_not':self.__conflict,
             'Bet_or_quit':player.get_current_bet_or_not(),
             'Win_or_lose':self.__current_win_or_lose,
             'Chip':player.get_current_chip(),
@@ -122,9 +137,9 @@ class Game:
             # Win or lose
             self.__current_win_or_lose = random.choice(['win', 'lose'])
             if self.__current_win_or_lose == 'win':
-                trial_outcome = self.__current_gain
+                trial_outcome = self.__current_gain_outcome
             else:  # lose
-                trial_outcome = self.__current_loss  # self.__current_loss < 0
+                trial_outcome = self.__current_loss_outcome  # self.__current_loss_outcome < 0
             
             # Compute chip
             if self.__current_trial == (self.__n_trial - 1):
@@ -151,6 +166,7 @@ class Game:
                 player.change_current_incentive(__incentive)
             next_step = 'quit'
         
+        # Save the current trial after computing the chip
         self.__save_trial(player)
         if next_step == 'next trial':
             self.__current_trial += 1
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def whole_experiment(n_simulation):
         alpha = data.iat[candidate, 0]
         beta = data.iat[candidate, 1]
         player = Player(alpha, beta)
-        game = Game()
+        game = Game(exp_type = 0)
 
         # Initialize the game
         game.initialization()
@@ -32,23 +32,36 @@ def whole_experiment(n_simulation):
 
 def single_run_experiment(candidate):
     for run_num in range(6):
-        run_df = pd.DataFrame({
-            'Simulation':[],
+        run_df = pd.DataFrame({  # Store trial data
+            "Simulation":[],
+            'Run':[],
             'Trial':[],
+            'Gain_outcome':[],
+            'Loss_outcome':[],
+            'Gain_utility':[],
+            'Loss_utility':[],
+            'Probability':[],
+            'Conflict_or_not':[],
+            'Bet_or_quit':[],
+            'Win_or_lose':[],
+            'Chip':[],
+            'Incentive':[],
         })
-        df_append_index = 0
+        another_run_df = pd.DataFrame({
+            'Simulation':[],
+            'Trial':[]
+        })
 
-        for index in range(n_simulation):
+        for simulation in range(n_simulation):
             # Create the game and the player
             alpha = round(data.at[candidate, 'alpha'], 3)
             beta = round(data.at[candidate, 'beta'], 3)
             player = Player(alpha, beta)
-            if not less_initial_chip_amount:
-                game = Game(single_run = run_num, initial_chip_ratio = 1)
+            if less_initial_chip_amount:
+                game = Game(exp_type = 1, single_run = run_num, initial_chip_ratio = 1)
             else:
-                game = Game(single_run = run_num)
+                game = Game(exp_type = 1, single_run = run_num)
             initial_value = game.get_initial_value_list()[run_num]
-            game_break = False
 
             # Initialize the game
             game.initialization()
@@ -56,32 +69,31 @@ def single_run_experiment(candidate):
             # Start the game
             while game.get_current_run() < game.get_n_run():
                 game.update_lottery(player)
-                if not game.check_conflict(player):
-                    game_break = True
-                    break
 
             # Get the result
-            if not game_break:
-                game_result = game.get_result()
-                [index_of_last_trial] = game_result.tail(1)['Trial']
+            game_result = game.get_result()
+            
+            # Add how many simulations have been done to this trial's dataframe, and then combine it with the whole run data
+            n_index = game_result.shape[0]
+            game_result.insert(0, column = 'Simulation', value = [simulation for i in range(n_index)])
+            run_df = pd.concat([run_df, game_result], ignore_index = True)
 
-                if [game_result.tail(1)['Win_or_lose']] == 'win':
-                    trial_num = index_of_last_trial
-                else:  # Quit the run by running out of chips
-                    trial_num = index_of_last_trial - 1
-                trial_num += 1  # Make the number more intuitive (start from 1 instead of 0)
+            # Test
+            [index_of_last_trial] = game_result.tail(1)['Trial']
 
-                run_df = run_df.append({
-                    'Simulation':index + 1,
-                    'Trial':trial_num
-                }, ignore_index = True)
-                df_append_index += 1
+            if [game_result.tail(1)['Win_or_lose']] == 'win':
+                trial_num = index_of_last_trial
+            else:  # Quit the run by running out of chips
+                trial_num = index_of_last_trial - 1
+
+            another_run_df = another_run_df.append({
+                'Simulation':simulation,
+                'Trial':trial_num
+            }, ignore_index = True)
+
 
         # Save data
-        if df_append_index == 0:
-            continue
-
-        if data_type == data_type_list[0]:
+        if data_type == 0:
             folder_path = f'./simulation_data/candidate_criteria/player-{candidate}_alpha-{alpha}_beta-{beta}'
         else:
             folder_path = f'./simulation_data/real_subject/player-{subject_list[candidate]}_alpha-{alpha}_beta-{beta}'
@@ -90,36 +102,42 @@ def single_run_experiment(candidate):
             os.makedirs(folder_path)  # Make directory if the directory is not found
         file_path = folder_path + f'/initial_value-{initial_value}.csv'
         run_df.to_csv(file_path, index = False)
+        another_file_path = f'{folder_path}/previous_format_initial_value-{initial_value}.csv'
+        another_run_df.to_csv(another_file_path, index = False)
     print(f'Candidate {candidate} is done!')
 
 # Set up
-task_type_list = ['whole_experiment', 'single_run_experiment']
-data_type_list = ['criteria', 'real_subject']
+exp_type_dic = {'whole_experiment':0, 'single_run_experiment':1}
+data_type_dic = {'criteria':0, 'real_subject':1}
 
 # Set the experiment type and the data type
-task_type = task_type_list[1]  # Choose to simulate the whole experiment or just one run
-data_type = data_type_list[1]  # Choose whether the alpha and beta come from real subject or be made up by "candidate_criteria.py"
+exp_type = exp_type_dic['single_run_experiment']  # Choose to simulate the whole experiment, just one run or 
+data_type = data_type_dic['real_subject']  # Choose whether the alpha and beta come from real subject or be made up by "candidate_criteria.py"
 less_initial_chip_amount = False  # Let true denote that the initial chip in every run is as many as initial gain
 
 # Read candidate data
-if data_type == data_type_list[0]:  # Choose "criteria"
+if data_type == 0:  # Choose "criteria"
     data = pd.read_csv('./resource/candidate_criteria.csv')
 else:  # Choose "real_subject"
     data = pd.read_csv('./resource/subject-selecting.csv')
     subject_list = data['subject']
 
-n_simulation = 100
-n_candidate = len(data)
+n_simulation = 10
+# n_candidate = len(data)
+n_candidate = 10
 
 # Run the simulation
 if __name__ == '__main__':
 
     # Mutliprocessing (using different cpu)
     pool = mp.Pool()
-    if task_type == task_type_list[0]:  # Choose "whole_experiment"
+    if exp_type == 0:  # Choose "whole_experiment"
         pool.map(whole_experiment, range(n_simulation))
     else:  # Choose "single_run_experiment"
         pool.map(single_run_experiment, range(n_candidate))
 
     # Inform us when the simulation is done
     print('The simulation is finished')
+
+# # Only for testing
+# single_run_experiment(6)
